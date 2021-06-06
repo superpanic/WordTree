c57b036c92d203809b256f6831e46a810468d4ad
fredrik josefsson <fredrik@teenage.engineering>
2021-06-06 16:45:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordTree {
+	class Program {
+		static void Main(string[] args) {
+
+			List<string> wordList;
+			
+			wordList = new List<string> {
+				"banana",
+				"calming",
+				"astronomy",
+				"astrology",
+				"bandana",
+				"calling",
+				"hidden",
+				"asthma",
+				"astronaut",
+				"ape",
+				"monkey"
+			};
+			wordList.Sort();
+
+			bool isRoot = true;
+			WordNode rootNode = new WordNode(isRoot);
+			
+			for (int i = 0; i < wordList.Count; i++) {
+				rootNode.AddWord(wordList[i]);
+			}
+
+			SearchString(rootNode, "ape");
+			SearchString(rootNode, "banana");
+			SearchString(rootNode, "coconut");
+
+		}
+
+		static void SearchString(WordNode rootNode, string s) {
+			int r = rootNode.ReadWord(s);
+			if (r > 0) {
+				Console.WriteLine("word: " + s + " found! (" + r + " chars long)");
+			} else {
+				Console.WriteLine("word: " + s + " not found!");
+			}
+		}
+
+	}
+}

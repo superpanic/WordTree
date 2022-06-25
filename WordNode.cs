2fbc890e0235668ec5cc803f66fa946c3fe02c0d
superpanic <superpanic@gmail.com>
2022-06-25 15:30:07 +0200

    added weight to node

--- WordNode.cs
+++ WordNode.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace WordTree {
+namespace WordWrap {
 	// a trie data structure for a large wordlist
 	// words need to be added in sorted order from a-z
 
 	public class WordNode {
-		private bool IsRoot = false;
-		private bool IsEndOfWord = false;
+		private readonly bool IsRoot = false;
+		public bool IsEndOfWord = false;
 		public char Letter;
-		private List<WordNode> NodeList;
+		public ushort weight = 0;
+		public List<WordNode> NodeList;
 
 		/* constructors */
 		public WordNode(bool isRoot) {
@@ -33,31 +34,35 @@ namespace WordTree {
 				if (word[0] != Letter) throw new ArgumentException("First char of word does not match node letter!");
 				if (word.Length == 1) {
 					IsEndOfWord = true;
+					weight++;
 					return;
 				}
 				word = word.Substring(1); // strip first char from word, it's already asigned to this nodes Letter (if not root)
 			}
-				
+
 			char firstLetter = word[0];
 
 			for (int i = 0; i < NodeList.Count; i++) {
 
 				if (NodeList[i].Letter == firstLetter) { // any match with current char?
 					NodeList[i].AddWord(word);
+					weight++;
 					return;
 				}
 
 				if (firstLetter < NodeList[i].Letter) {
 					WordNode wn = new WordNode(word);
 					NodeList.Insert(i, wn);
+					weight++;
 					return;
 				}
-					
+
 			}
 
 			// we reached the end of the node list (or node list is empty):
 			WordNode w = new WordNode(word);
 			NodeList.Add(w);
+			weight++;
 			return;
 		}
 
@@ -83,5 +88,6 @@ namespace WordTree {
 			}
 			return 0;
 		}
+
 	}
 }
